f05c78484ec5b40980d99ea0b6ba0bbe0941d77a
Li, Hui <pub@halfroad.com>
2022-08-10 14:17:03 +0800

    Train the model of Keras on TFHUB

--- Chapters/10/10.4/DogSpeciesRecognitionTensorFlowHubKeras.py
+++ Chapters/10/10.4/DogSpeciesRecognitionTensorFlowHubKeras.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import keras
 import tensorflow_hub as hub
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
 import os
 
 def DownloadImageNetDogs():
@@ -33,27 +34,47 @@ def Prepare():
     print("A comprehensive train requires {} times.".format(stepsPerEpoch))
     
     return imageFlow, inputImageSize, stepsPerEpoch
-    
-def LoadPretrainModel(x):
-    
-    # featureExtractorURL = "https://tfhub.dev/google/imagenet/mobilenet_v2_100_224/feature_vector/5"
-    featureExtractorURL = "imagenet_mobilenet_v2_100_224_feature_vector_5"
-    featureExtractorModule = hub.Module(featureExtractorURL)
-    
-    return featureExtractorModule(x)
 
 def CreateModel(imageFlow, inputImageSize):
     
-    featuresExtractorLayer = keras.layers.Lambda(LoadPretrainModel, input_shape = inputImageSize + (3, ))
-    featuresExtractorLayer.trainable = False
-    
-    model = tf.keras.Sequential([featuresExtractorLayer, tf.keras.layers.Dense(imageFlow.num_classes, activation="softmax")])
-    
+    featureExtractorURL = "https://tfhub.dev/google/imagenet/mobilenet_v2_100_224/feature_vector/5"
+
+    layers = [
+        hub.KerasLayer(handle = featureExtractorURL, input_shape = inputImageSize + (3, ), trainable = False, name = "mobilenet_embedding"),
+        tf.keras.layers.Dense(imageFlow.num_classes, activation="softmax")
+
+        ]
+    model = tf.keras.Sequential(layers, name = "DogSpeciesFeatures")
+        
     print(model.summary())
     
     return model
 
+
+class CollectionBatchStats(tf.keras.callbacks.Callback):
+        
+        def __init__(self):
+            
+            self.batch_losses = []
+            self.batch_accuracies = []
+            
+            def onBatchEnd(self, batch, logs = None):
+                
+                self.batch_losses.append(logs["loss"])
+                self.batch_accuracies.append(logs["accuracy"])
+
+def TrainModel(model, imageFlow, stepsPerEpoch):
+    
+    model.compile(optimizer = keras.optimizers.Adam(), loss = "categorical_crossentropy", metrics = ["accuracy"])
+    
+    batchStats = CollectionBatchStats()
+    
+    model.fit((item for item in imageFlow), epochs = 5, steps_per_epoch = stepsPerEpoch, callbacks = [batchStats])
+    
+
 # DownloadImageNetDogs()
 imageFlow, inputImageSize, stepsPerEpoch = Prepare()
-CreateModel(imageFlow, inputImageSize)
+model = CreateModel(imageFlow, inputImageSize)
+
+TrainModel(model, imageFlow, stepsPerEpoch)
 # LoadPretrainModel(1)
