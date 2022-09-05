bbf6809cd8a7e5a1a11b3afb08277951c88ea8c1
Li, Hui <pub@halfroad.com>
2022-09-05 23:32:05 +0800

    Plot the trained images

--- Chapters/20/20.2/ImageSuperResolutionDeepConvolutionalGenerativeAdversarialNetwork.py
+++ Chapters/20/20.2/ImageSuperResolutionDeepConvolutionalGenerativeAdversarialNetwork.py
@@ -2,6 +2,25 @@
 
 ResNet: Deep Residual Networks
 
+python3 srez_main.py --run train --batch_size 16 --checkpoint_dir ../Checkpoint --checkpoint_period 100 --dataset dataset --epsilon 1e-8 --gene_l1_factor 0.90 --learning_beta1 0.5 --learning_rate_start 0.00020 --learning_rate_life 5000 --sample_size 168 --summary_period 10 --random_seed 0 --test_vectors 16 --random_seed 0 --test_vectors 16 --train_dir Trains --train_time 20
+
 '''
 
-python3 srez_main.py --run train --batch_size 16 --checkpoint_dir ../Checkpoint --checkpoint_period 100 --dataset dataset --epsilon 1e-8 --gene_l1_factor 0.90 --learning_beta1 0.5 --learning_rate_start 0.00020 --learning_rate_life 5000 --sample_size 168 --summary_period 10 --random_seed 0 --test_vectors 16 --random_seed 0 --test_vectors 16 --train_dir Trains --train_time 20
+import matplotlib.pyplot as plt
+
+from matplotlib import image
+
+def PlotImage(path):
+    
+    figure, ax = ply.subplots()
+    
+    figure.set_size_inches(8, 8)
+    
+    _image = image.imread(path)
+    
+    ax.grid(False)
+    
+    plt.show()
+    
+PlotImage("../Exclusion/srez/Trains/No_10.000000_trained_out.png")
+
